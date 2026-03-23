cbbef451cfc036f7e60b1c7fd6ab3960df4c2500
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-03-22 22:31:17 -0300

    basic movement made, but now making the central rule of the game

--- main.py
+++ main.py
@@ -37,14 +37,51 @@ window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 # Iniciar o jogo
 snake = Tile(5*TILE_SIZE, 5*TILE_SIZE) # Célula inicial da cobra
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE) # Célula inicial da comida
+velocity_x = 0
+velocity_y = 0
+
+def change_direction(event):
+    # print(event.keysym) #  Mostra as teclas que pressiono
+    global velocity_x, velocity_y
+
+    if (event.keysym == "Up"):
+        velocity_x = 0
+        velocity_y = -1
+    elif (event.keysym == "Down"):
+        velocity_x = 0
+        velocity_y = 1
+    elif (event.keysym == "Left"):
+        velocity_x = -1
+        velocity_y = 0
+    elif (event.keysym == "Right"):
+        velocity_x = 1
+        velocity_y = 0
+
+
+# Função para mover a cobra
+def move():
+    global snake
+
+    snake.x += velocity_x * TILE_SIZE
+    snake.y += velocity_y * TILE_SIZE
+
 
 def draw():
     global snake
+    move()
+
+    canvas.delete("all")
 
     # Desenhando a cobra
     canvas.create_rectangle(snake.x, snake.y, snake.x + TILE_SIZE, snake.y + TILE_SIZE, fill="lime green")
+
+    # Desenhando a comida
+    canvas.create_rectangle(food.x, food.y, food.x + TILE_SIZE, food.y + TILE_SIZE, fill="red")
     
     window.after(100, draw) # Chama a função draw a cada 100ms
 
 draw()
+
+window.bind("<KeyRelease>", change_direction)
 window.mainloop()
