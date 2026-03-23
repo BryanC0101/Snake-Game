b138c314b1476a95f6e8bb120c4d08ba2a222af4
Bryan Cauê Vaz <bcaue177@gmail.com>
2026-03-22 22:01:49 -0300

    window created

---
+++ main.py
@@ -0,0 +1,22 @@
+import tkinter
+import random
+
+ROWS = 25
+COLS = 25
+TITLE_SIZE = 25
+
+WINDOW_WIDTH = TITLE_SIZE * ROWS
+WINDOW_HEIGHT = TITLE_SIZE * COLS
+
+# Criar a janela
+window = tkinter.Tk()
+window.title("Snake")
+window.resizable(False, False)
+
+# Aqui é onde o jogo será desenhado
+canvas = tkinter.Canvas(window, bg = "black", width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+canvas.pack()
+window.update()
+
+
+window.mainloop()
